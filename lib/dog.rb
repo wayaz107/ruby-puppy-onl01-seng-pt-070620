da236c9850f50854ac197e638fdefa52a8a41496
Warda Ayaz <wayaz107@gmail.com>
2020-07-27 18:34:20 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,27 @@
 # Add your code here
+class Dog
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    save 
+  end 
+
+  def save
+    @@all << self
+  end 
+
+  def self.clear_all
+    @@all.clear
+  end 
+
+  def self.all
+    @@all
+  end 
+  
+  def self.print_all
+    all.each do |dogs|
+      dogs
+    end 
+  end 
+end 
